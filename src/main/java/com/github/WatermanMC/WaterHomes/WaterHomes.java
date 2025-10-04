6c4da88c2a5849b61088c966b1608f277c524723
WatermanMC <lozadanoah24@gmail.com>
2025-10-04 17:50:25 +0800

    Add files via upload

---
+++ src/main/java/com/github/WatermanMC/WaterHomes/WaterHomes.java
@@ -0,0 +1,33 @@
+package com.github.WatermanMC.WaterHomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WaterHomes extends JavaPlugin {
+    
+    private HomeManager homeManager;
+    private ConfigManager configManager;
+    private CommandManager commandManager;
+    
+    @Override
+    public void onEnable() {
+        this.configManager = new ConfigManager(this);
+        this.homeManager = new HomeManager(this);
+        this.commandManager = new CommandManager(this);
+        configManager.loadConfigs();
+        commandManager.registerCommands();
+        getLogger().info("Enabling WaterHomes...");
+    }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling WaterHomes...");
+    }
+    
+    public HomeManager getHomeManager() {
+        return homeManager;
+    }
+    
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+}
